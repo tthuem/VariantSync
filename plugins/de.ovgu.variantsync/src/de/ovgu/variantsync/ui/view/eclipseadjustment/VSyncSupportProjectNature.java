b1c6b4ca92544947be400f0d02895bf4eac9bbdd
Tristan Pfofe <pfofe@ckc.de>
2016-09-04 19:49:14 +0200

    SPLC Demo #1: fixed

--- plugins/de.ovgu.variantsync/src/de/ovgu/variantsync/ui/view/eclipseadjustment/VSyncSupportProjectNature.java
+++ plugins/de.ovgu.variantsync/src/de/ovgu/variantsync/ui/view/eclipseadjustment/VSyncSupportProjectNature.java
@@ -1,5 +1,8 @@
 package de.ovgu.variantsync.ui.view.eclipseadjustment;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,7 +11,9 @@ import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -26,8 +31,7 @@ import de.ovgu.variantsync.utilities.LogOperations;
  */
 public class VSyncSupportProjectNature implements IProjectNature {
 
-	public static final String NATURE_ID = VariantSyncConstants.PLUGIN_ID
-			+ ".vsyncSupportProjectNature";
+	public static final String NATURE_ID = VariantSyncConstants.PLUGIN_ID + ".vsyncSupportProjectNature";
 	private IProject project;
 
 	/*
@@ -37,8 +41,9 @@ public class VSyncSupportProjectNature implements IProjectNature {
 	 */
 	@Override
 	public void configure() throws CoreException {
-		VariantSyncPlugin.getDefault().logMessage(
-				"Project \"" + project.getName() + "\" is supported.");
+		project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+		refreshProject();
+		VariantSyncPlugin.getDefault().logMessage("Project \"" + project.getName() + "\" is supported.");
 	}
 
 	/*
@@ -48,8 +53,9 @@ public class VSyncSupportProjectNature implements IProjectNature {
 	 */
 	@Override
 	public void deconfigure() throws CoreException {
-		VariantSyncPlugin.getDefault().logMessage(
-				"Project \"" + project.getName() + "\" is not supported.");
+		project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+		refreshProject();
+		VariantSyncPlugin.getDefault().logMessage("Project \"" + project.getName() + "\" is not supported.");
 		IFolder folder = project.getFolder(VariantSyncConstants.ADMIN_FOLDER);
 		folder.delete(true, false, null);
 		update();
@@ -89,8 +95,7 @@ public class VSyncSupportProjectNature implements IProjectNature {
 		try {
 			description = project.getDescription();
 		} catch (CoreException e) {
-			LogOperations.logError(
-					"Project description could not be retrieved.", e);
+			LogOperations.logError("Project description could not be retrieved.", e);
 			return;
 		}
 
@@ -108,7 +113,9 @@ public class VSyncSupportProjectNature implements IProjectNature {
 
 		// Save the description.
 		try {
-			project.setDescription(description, null);
+			project.setDescription(description, new NullProgressMonitor());
+			project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+			refreshProject();
 		} catch (CoreException e) {
 			LogOperations.logError("Project description could not be set.", e);
 		}
@@ -118,8 +125,7 @@ public class VSyncSupportProjectNature implements IProjectNature {
 		try {
 			return project.isOpen() && project.hasNature(NATURE_ID);
 		} catch (CoreException e) {
-			LogOperations.logError(
-					"Project nature support could not be checked.", e);
+			LogOperations.logError("Project nature support could not be checked.", e);
 			return false;
 		}
 	}
@@ -136,8 +142,7 @@ public class VSyncSupportProjectNature implements IProjectNature {
 		try {
 			description = project.getDescription();
 		} catch (CoreException e) {
-			LogOperations.logError(
-					"Project description could not be retrieved.", e);
+			LogOperations.logError("Project description could not be retrieved.", e);
 			return;
 		}
 
@@ -156,19 +161,32 @@ public class VSyncSupportProjectNature implements IProjectNature {
 		// Save the description.
 		try {
 			project.setDescription(description, null);
+			project.refreshLocal(IResource.DEPTH_INFINITE, null);
+			refreshProject();
 		} catch (CoreException e) {
 			LogOperations.logError("Project description could not be set.", e);
 		}
 	}
 
+	private static void refreshProject() {
+		try {
+			Robot r = null;
+			r = new Robot();
+			int keyCode = KeyEvent.VK_F5;
+			r.keyPress(keyCode);
+			r.keyRelease(keyCode);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
+
 	// update the view
-	private void update() {
+	private static void update() {
 		Display display = VariantSyncPlugin.getStandardDisplay();
 		if (!display.isDisposed()) {
 			display.asyncExec(new Runnable() {
 				public void run() {
-					IWorkbenchWindow window = PlatformUI.getWorkbench()
-							.getActiveWorkbenchWindow();
+					IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 					if (window == null) {
 						return;
 					}
@@ -177,9 +195,7 @@ public class VSyncSupportProjectNature implements IProjectNature {
 						return;
 					}
 					if (page.findView(ResourceChangesView.ID) != null) {
-						((ResourceChangesView) page
-								.findView(ResourceChangesView.ID))
-								.refreshTree();
+						((ResourceChangesView) page.findView(ResourceChangesView.ID)).refreshTree();
 					}
 				}
 			});

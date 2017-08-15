a17786604a1a505369e31f21526755f19a8212ea
Kogoro <ch.sontag@gmail.com>
2017-08-15 17:14:46 +0200

    Inital maven project structure

---
+++ de.tubs.variantsync.core/src/de/tubs/variantsync/core/CorePlugin.java
@@ -0,0 +1,50 @@
+package de.tubs.variantsync.core;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class CorePlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "de.tubs.variantsync.core"; //$NON-NLS-1$
+
+	// The shared instance
+	private static CorePlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public CorePlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static CorePlugin getDefault() {
+		return plugin;
+	}
+
+}

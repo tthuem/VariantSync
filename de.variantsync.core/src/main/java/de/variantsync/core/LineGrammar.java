bf259dc99b89848bb8de16096fc947790678ff62
wurstbroteater <fleri94@web.de>
2021-01-06 15:51:26 +0100

    first try to make attributed LineGrammar Enum

--- de.variantsync.core/src/main/java/de/variantsync/core/LineGrammar.java
+++ de.variantsync.core/src/main/java/de/variantsync/core/LineGrammar.java
@@ -1,5 +1,37 @@
 package de.variantsync.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum LineGrammar {
-    Directory, TextFile, BinaryFile, Line;
+	Directory, TextFile, BinaryFile, Line;
+
+	private List<Object> addAttributes;
+
+	LineGrammar() {
+		addAttributes = new ArrayList<>();
+	}
+
+	LineGrammar(List<Object> addAttributes) {
+		this.addAttributes = addAttributes;
+	}
+
+	public List<Object> getAttributes() {
+		return addAttributes;
+	}
+
+	private boolean isValidChild(LineGrammar parent, LineGrammar child) {
+		if (parent == LineGrammar.Directory) {
+			//Dir can't have line as child
+			return child != LineGrammar.Line;
+		} else if (parent == LineGrammar.TextFile || parent == LineGrammar.BinaryFile) {
+			//File can't have dir or file as child
+			return child == LineGrammar.Line;
+		} else {
+			//Line is always leaf node
+			return false;
+		}
+
+	}
+
 }

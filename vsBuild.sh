3e2ec0af8a94f9c61dbf68a0bf4bc51e1c937ed5
wurstbroteater <fleri94@web.de>
2021-02-26 14:22:11 +0100

    fixed vsBuild

--- vsBuild.sh
+++ vsBuild.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 cd de.variantsync.core
-mvn clean verify
-eLevel = $?
+mvn clsean verify
+eLevel=$?
 
-if [eLevel -eq 0] 
+if [ $eLevel -eq 0 ] 
 then
    #Building library was successful
+   cp target/de.variantsync.core-0.0.3-SNAPSHOT.jar ../de.tubs.variantsync.core/lib/
    cd ..
    mvn clean verify
-   return $?
+   exit $?
 else
    #Building library was not successful
-   return eLevel
+   exit $eLevel
 fi   

769ed4f83a577eb7c8434babc6c6bed0419faa9a
Wurstbroteater <fleri94@web.de>
2021-03-22 14:41:55 +0100

    refactored AST test and toString test

--- de.variantsync.core/src/test/java/de/variantsync/core/ASTTest.java
+++ de.variantsync.core/src/test/java/de/variantsync/core/ASTTest.java
@@ -1,7 +1,9 @@
 package de.variantsync.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Before;
@@ -13,45 +15,75 @@ import de.variantsync.core.ast.LineGrammar;
 //TODO. Add compare method to AST and then compare ASTs with this method.
 
 public class ASTTest {
+    AST<LineGrammar, String> root;
+    AST<LineGrammar, String> mainDir;
+    AST<LineGrammar, String> testDir;
+    AST<LineGrammar, String> mainJava;
+    int lineIndex = 0; // only for toString testing
 
-	AST<LineGrammar, String> ast;
-	AST<LineGrammar, String> mainJava;
+    @Before
+    public void setup() {
+        root = new AST<>(LineGrammar.Directory, "src");
+        mainDir = new AST<>(LineGrammar.Directory, "main");
+        testDir = new AST<>(LineGrammar.Directory, "test");
+        mainJava = new AST<>(LineGrammar.TextFile, "Main.java");
+        final AST<LineGrammar, String> emptyJava = new AST<>(LineGrammar.TextFile, "Empty.java");
+        final AST<LineGrammar, String> emptyTestJava = new AST<>(LineGrammar.TextFile, "EmptyTest.java");
+        root.addChild(testDir);
+        testDir.addChild(emptyTestJava);
+        root.addChild(mainDir);
+        mainDir.addChild(mainJava);
+        mainDir.addChild(emptyJava);
 
-	@Before
-	public void setup() {
-		ast = new AST<>(LineGrammar.Directory, "src");
-		final AST<LineGrammar, String> mainDir = new AST<>(LineGrammar.Directory, "main");
-		final AST<LineGrammar, String> testDir = new AST<>(LineGrammar.Directory, "test");
-		mainJava = new AST<>(LineGrammar.TextFile, "Main.java");
-		final AST<LineGrammar, String> emptyJava = new AST<>(LineGrammar.TextFile, "Empty.java");
-		final AST<LineGrammar, String> emptyTestJava = new AST<>(LineGrammar.TextFile, "EmptyTest.java");
-		ast.addChild(testDir);
-		testDir.addChild(emptyTestJava);
-		ast.addChild(mainDir);
-		mainDir.addChild(mainJava);
-		mainDir.addChild(emptyJava);
+        mainJava.addChildren(
+                Arrays.asList(new AST<>(LineGrammar.Line, "public class Main {"),
+                        new AST<>(LineGrammar.Line, "    public static void main(String[] args)"),
+                        new AST<>(LineGrammar.Line, "        System.out.println(\"Hello World\");"),
+                        new AST<>(LineGrammar.Line, "    }"),
+                        new AST<>(LineGrammar.Line, "}"))
+        );
+    }
 
-		mainJava.addChildren(
-				Arrays.asList(new AST<>(LineGrammar.Line, "public class Main {"), new AST<>(LineGrammar.Line, "    public static void main(String[] args)"),
-						new AST<>(LineGrammar.Line, "        System.out.println(\"Hello World\");"), new AST<>(LineGrammar.Line, "    }"),
-						new AST<>(LineGrammar.Line, "}")));
-	}
+    @Test
+    public void sizeTest() {
+        assertEquals(11, root.size());
+        root = new AST<>(null, null);
+        assertEquals(1, root.size());
+    }
 
-	@Test
-	public void sizeTest() {
-		assertEquals(11, ast.size());
-		ast = new AST<>(null, null);
-		assertEquals(1, ast.size());
-	}
+    @Test
+    public void toStringTest() {
+        String[] lines = root.toString().split(String.format("%n"));
+        assertEquals(11, lines.length);
+        // test root values
+        String[] rootAttributes = lines[0].split(" ");
+        assertEquals(4, rootAttributes.length);
+        // in general checking like this is not possible due to the nature of AST.INDENT_STRING
+        // but root line does not contain AST.INDENT_STRING
+        assertEquals(rootAttributes[0], root.getType().toString()); //Type
+        assertEquals(rootAttributes[1], root.getValue()); // Value
+        assertEquals(rootAttributes[2], "uuid:");
+        assertEquals(rootAttributes[3], ((Long) root.getId().getMostSignificantBits()).toString()); // UUID
 
-	@Test
-	public void toStringTest() {
-		final String expected = "Directory src Depth: 0\n" + "    ├─Directory test Depth: 1\n" + "    │     └─ TextFile EmptyTest.java Depth: 2\n"
-			+ "    └─Directory main Depth: 1\n" + "          ├─ TextFile Main.java Depth: 2\n" + "          │      ├─ Line public class Main { Depth: 3\n"
-			+ "          │      ├─ Line     public static void main(String[] args) Depth: 3\n"
-			+ "          │      ├─ Line         System.out.println(\"Hello World\"); Depth: 3\n" + "          │      ├─ Line     } Depth: 3\n"
-			+ "          │      └─ Line } Depth: 3\n" + "          └─ TextFile Empty.java Depth: 2\n";
-		assertEquals(expected, ast.toString());
-	}
+        // check subtrees
+        printSubTree(root, lines);
+    }
 
+    private void printSubTree(AST<LineGrammar, String> node, String[] lines) {
+        lineIndex++;
+        for (final AST<LineGrammar, String> child : node.getSubtree()) {
+            System.out.printf("%s %s %3$d%n", child.getType().toString(), child.getValue(), child.getId().getMostSignificantBits());
+            String line = lines[lineIndex];
+            assertTrue(line.contains(AST.INDENT_STRING));
+            assertTrue(line.contains(AST.NEXT_SEPARATOR)
+                    || line.contains(AST.NEXT_ACT_SEPARATOR)
+                    || line.contains(AST.LAST_SEPARATOR)
+            );
+            assertTrue(line.contains(child.getType().toString()));
+            assertTrue(line.contains(child.getValue()));
+            assertTrue(line.contains(((Long) child.getId().getMostSignificantBits()).toString()));
+
+            printSubTree(child, lines);
+        }
+    }
 }
